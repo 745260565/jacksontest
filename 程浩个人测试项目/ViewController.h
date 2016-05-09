b790f09207d5292c4e117991dfaf9c57de4dc347
ch <ch@harbin6666demac-mini.local>
2016-05-09 13:57:54 +0800

    Initial Commit

---
+++ 程浩个人测试项目/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  程浩个人测试项目
+//
+//  Created by ch on 16/5/9.
+//  Copyright © 2016年 id. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 程浩个人测试项目/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  程浩个人测试项目
+//
+//  Created by ch on 16/5/9.
+//  Copyright © 2016年 id. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
